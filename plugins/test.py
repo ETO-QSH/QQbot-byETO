2b056d7c91f11dfe82ee6dfdbe45bc9e2465078c
ETO-QSH <2373204754@qq.com>
2025-09-08 15:00:39 +0800

    ETO

--- plugins/test.py
+++ plugins/test.py
@@ -1,4 +1,5 @@
-import http.client, json
+import json
+from http.client import HTTPConnection
 
 
 audio = "https://raw.githubusercontent.com/ETO-QSH/QQbot-byETO/refs/heads/main/links/水瀬いのり%20久保ユリカ%20-%20Endless%20Journey.flac"
@@ -24,7 +25,7 @@ headers = {
     'Authorization': 'Bearer ETO'
 }
 
-conn = http.client.HTTPConnection("127.0.0.1", 3000)
+conn = HTTPConnection("127.0.0.1", 3000)
 conn.request("POST", "/send_group_msg", payload, headers)
 res = conn.getresponse()
 print(res.read().decode())
