08e52f06e55d6120516904410196fbf7424527b7
ETO-QSH <2373204754@qq.com>
2025-09-08 14:48:00 +0800

    ETO

--- plugins/test.py
+++ plugins/test.py
@@ -1,6 +1,8 @@
 import http.client, json
 
-conn = http.client.HTTPConnection("127.0.0.1", 3000)
+
+audio = "https://raw.githubusercontent.com/ETO-QSH/QQbot-byETO/refs/heads/main/links/水瀬いのり%20久保ユリカ%20-%20Endless%20Journey.flac"
+image = "https://raw.githubusercontent.com/ETO-QSH/QQbot-byETO/refs/heads/main/links/少女终末旅行_1x1.jpg"
 
 payload = json.dumps({
     "group_id": 981535936,
@@ -8,11 +10,11 @@ payload = json.dumps({
         "type": "music",
         "data": {
             "type": "custom",
-            "url": "https://music.qq.com",
-            "audio": "D:/Images/水瀬いのri久保ユリカ-EndlessJourney.flac",
+            "url": "https://github.com/ETO-QSH/QQbot-byETO",
+            "audio": audio,
             "title": "Endless Journey",
             "content": "少女終末旅行-EP",
-            "image": "D:/Desktop/Desktop/image/少女终末旅行.jpg"
+            "image": image
         }
     }]
 })
@@ -22,6 +24,7 @@ headers = {
     'Authorization': 'Bearer ETO'
 }
 
+conn = http.client.HTTPConnection("127.0.0.1", 3000)
 conn.request("POST", "/send_group_msg", payload, headers)
 res = conn.getresponse()
 print(res.read().decode())
